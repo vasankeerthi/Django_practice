4dc3ad92d5b34f66b2d918a7eaec15c5eaa2de4c
vasankeerthi <49607292+vasankeerthi@users.noreply.github.com>
2021-09-10 12:21:24 +0530

    added new feature in the form of a new app for sending mail and verifing the birthday date of the users

--- birthday/feature1/views.py
+++ birthday/feature1/views.py
@@ -3,7 +3,7 @@ from django.contrib.auth.models import User,auth
 from django.contrib.auth.decorators import login_required
 from django.contrib import messages
 from .models import Birthday
-# Create your views here.
+
 def index(request):
     return render(request ,'index.html')
 
