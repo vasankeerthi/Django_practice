b35bbf891379b738194862c42a7cd3ad74fb5427
vasankeerthi <49607292+vasankeerthi@users.noreply.github.com>
2021-09-11 21:43:37 +0530

    Delete firstpro directory

--- firstpro/feature1/apps.py
+++
@@ -1,5 +0,0 @@
-from django.apps import AppConfig
-
-
-class Feature1Config(AppConfig):
-    name = 'feature1'
--- firstpro/feature1/urls.py
+++
@@ -1,8 +0,0 @@
-from django.urls import path
-from . import views
-
-urlpatterns=[
-    path('',views.index,name='index'),
-    path('reg',views.user_reg,name='register'),
-    path('login',views.user_login,name="login"),
-]
